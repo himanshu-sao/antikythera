b502323116a3fa332bd9a19a41f120be3c1cd10a
Himanshu Shekhar Sao <himanshusao@Himanshus-MacBook-Pro.local>
2026-06-07 12:18:34 -0500

    Remove dist directory from repository and add to .gitignore

--- dist/assets/chunk-55IACEB6-UeHWDARU.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,d as o}from"./index-CeN5J5hB.js";var d=a((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
